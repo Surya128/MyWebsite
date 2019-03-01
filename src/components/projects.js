9c3df4b98204094fdc51ae0c1107b15b35093dad
Surya <chittivh@mail.uc.edu>
2019-03-01 12:35:54 -0500

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -73,7 +73,7 @@ class Projects extends Component {
 - To build this application , Model 2 (MVC) architecture is used to make the navigation control, ease of access and testing better.<br />
               </CardText>
             <CardActions border>
-              <Button colored><a href = "https://github.com/Surya128/festival-reg-system" >GitHub</a></Button>
+              <Button colored><a href = "https://github.com/Surya128/festival-reg-system" target="_blank" rel="noopener noreferrer">GitHub</a></Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -84,8 +84,10 @@ class Projects extends Component {
     } else if(this.state.activeTab === 2) {
       return (
         <div className="projects-grid">
+
+
            <Card shadow={5} style={{width: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#000000', height: '200px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png) center / cover'}} >Re-ranking the comments for questions in Community Question Answering using SVM Classifier</CardTitle>
+            <CardTitle style={{color: '#000000', height: '300px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png) center / cover'}} >Re-ranking the comments for questions in Community Question Answering using SVM Classifier</CardTitle>
             <CardText>
             - Developed a project which ranks all the comments for a question according to their relevance. <br />
             - Preprocessing is done on the question-comments dataset and the necessary features are extracted using semantic word embeddings.<br />
@@ -94,12 +96,33 @@ class Projects extends Component {
             - Coding part of the project is done in python using NLTK.
               </CardText>
             <CardActions border>
-              <Button colored><a href = "https://github.com/Surya128/CQA" >GitHub</a></Button>
+              <Button colored><a href = "https://github.com/Surya128/CQA" target="_blank" rel="noopener noreferrer">GitHub</a></Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
+
+
+        <Card shadow={5} style={{width: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#000000', height: '425px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png) center / cover'}} >Development of Online Auction System using bootstrap and django</CardTitle>
+            <CardText>
+            - Designed and Developed a web based auction system that satisfies the requirements of ease of use, adaptability of changing requirements, maintainable and has a scope of reusability.<br />
+Front end : Bootstrap, Python , jQuery<br/>
+Back end : Mysql<br/>
+Framework : Django<br/>
+              </CardText>
+            <CardActions border>
+              <Button colored><a href = "https://github.com/Surya128/Auction_system" target="_blank" rel="noopener noreferrer">GitHub</a></Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+
+
+
         </div>
       )
     } else if(this.state.activeTab === 3) {
