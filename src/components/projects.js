6b1d5dc6dcb971add99932dcdd348597bdd692da
Surya <chittivh@mail.uc.edu>
2019-03-01 12:20:49 -0500

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -66,12 +66,14 @@ class Projects extends Component {
       return (
         <div className="projects-grid">
            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#000000', height: '176px', background: 'url(https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/eb7e73a0-96da-11e6-b385-00163ed833e7/4013307709/apple-java-for-mac-screenshot.png) center / cover'}} >Java Project #1</CardTitle>
+            <CardTitle style={{color: '#000000', height: '176px', background: 'url(https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/eb7e73a0-96da-11e6-b385-00163ed833e7/4013307709/apple-java-for-mac-screenshot.png) center / cover'}} >Development of Festival Registration System using Spring MVC and Servlets</CardTitle>
             <CardText>
-              Project Description
+            - Developed a web application where a user can view, edit their details, register and search one or more events. Admin can view , update , delete , add , register an event/s.<br />
+- Authentication and authorization of users is done on the server side and the validation of input fields is done on the client side.<br />
+- To build this application , Model 2 (MVC) architecture is used to make the navigation control, ease of access and testing better.<br />
               </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
+              <Button colored><a href = "https://github.com/Surya128/festival-reg-system" >GitHub</a></Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -82,13 +84,17 @@ class Projects extends Component {
     } else if(this.state.activeTab === 2) {
       return (
         <div className="projects-grid">
-           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#000000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png) center / cover'}} >Python Project #1</CardTitle>
+           <Card shadow={5} style={{width: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#000000', height: '200px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png) center / cover'}} >Re-ranking the comments for questions in Community Question Answering using SVM Classifier</CardTitle>
             <CardText>
-              Project Description
+            - Developed a project which ranks all the comments for a question according to their relevance. <br />
+            - Preprocessing is done on the question-comments dataset and the necessary features are extracted using semantic word embeddings.<br />
+            - Word2Vec models are trained on these data sources and cosine similarity is calculated for the resulting vectors.<br />
+            - SVM classifier is trained with the different values of the cost parameter and the one with best accuracy is selected.<br />
+            - Coding part of the project is done in python using NLTK.
               </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
+              <Button colored><a href = "https://github.com/Surya128/CQA" >GitHub</a></Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
