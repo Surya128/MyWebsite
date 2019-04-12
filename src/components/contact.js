f787c21236c1ea44b6dc6ffe02b21ce97d291ebc
Surya <chittivh@mail.uc.edu>
2019-04-11 20:12:38 -0500

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,52 +1,59 @@
-import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
-
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return(
+    return (
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
             <h2>Surya Prakash</h2>
             <img
-              src={ require('../images/my_pic.jpg') }
+              src={require("../images/my_pic.jpg")}
               alt="avatar"
-              style={{height: '250px'}}
-               />
-             
-
+              style={{ height: "250px" }}
+            />
           </Cell>
           <Cell col={6}>
-            <h2 style={{color:'white'}} align="center" >Contact Me</h2>
-            <hr/>
+            <h2 style={{ color: "white" }} align="center">
+              Contact Me
+            </h2>
+            <hr />
 
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'sans-serif', color:'white'}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                    (513) 641-8698
+                  <ListItemContent
+                    style={{
+                      fontSize: "30px",
+                      fontFamily: "sans-serif",
+                      color: "white"
+                    }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (513) 666-8055
                   </ListItemContent>
                 </ListItem>
-
-                
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'sans-serif', color:'white'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    suryaprakash128@outlook.com
+                  <ListItemContent
+                    style={{
+                      fontSize: "30px",
+                      fontFamily: "sans-serif",
+                      color: "white"
+                    }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    chittivh@mail.uc.edu
                   </ListItemContent>
                 </ListItem>
-
-                
-
               </List>
             </div>
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
